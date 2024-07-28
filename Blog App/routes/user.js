41f4e38ffba8c90cb7c8f597a6c87218b86ad24e
muhammetkilinc15 <mhmmtklnc07@gmail.com>
2024-07-28 15:46:13 +0300

    Category datas getting dynamicly

--- Blog App/routes/user.js
+++ Blog App/routes/user.js
@@ -3,49 +3,63 @@ const router = express.Router();
 const db = require("../data/db");
 
 const data = {
-    title: "Popüler Kurslar",
-    categories: ["Web Geliştirme", "Programlama", "Mobil Uygulamalar", "Veri Analizi", "Ofis Uygulamaları"],
+  title: "Popüler Kurslar",
+  categories: [
+    "Web Geliştirme",
+    "Programlama",
+    "Mobil Uygulamalar",
+    "Veri Analizi",
+    "Ofis Uygulamaları",
+  ],
 };
-
 
 // Bir kursa ait detaya gittik
 router.get("/blogs/:blogid", function (req, res) {
-    res.render("users/blogs-detail", {
-        blogid: req.params.blogid,
-        blog :  req.params.blog
-    });
+  res.render("users/blogs-detail", {
+    blogid: req.params.blogid,
+    blog: req.params.blog,
+  });
 });
-
 
 // Tüm Kursları blogs sayfasında listeledik
 router.get("/blogs", function (req, res) {
-    db.execute("select * from blog")
-        .then(result => {
-            res.render("users/blogs", {
-                title: "Tüm Kurslar",
-                categories: data.categories,
-                blogs: result[0]
-            });
+  db.execute("select * from blog where isApproved=1")
+    .then((result) => {
+      db.execute("select * from category")
+        .then((data) => {
+          res.render("users/blogs", {
+            title: "Tüm Kurslar",
+            categories: data[0],
+            blogs: result[0],
+          });
         })
-        .catch(err => {
-            console.log(err);
+        .catch((err) => {
+          console.log(err);
         });
+    })
+    .catch();
 });
-
 
 // Ana Sayfada Gerekli kursları listeledik
 router.get("/", function (req, res) {
-    db.execute("select * from blog")
-        .then(result => {
-            res.render("users/index", {             
+  db.execute("select * from blog where isApproved=1 and mainPage=1")
+    .then((result) => {
+        db.execute("select * from category")
+        .then(data=>{
+            res.render("users/index", {
                 title: "Popüler Kurslar",
-                categories: data.categories,
-                blogs: result[0]
-            });
+                categories: data[0],
+                blogs: result[0],
+              });
         })
-        .catch(err => {
-            console.log(err);
+        .catch(err=>{
+            console.log(err)
         });
+     
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router; // Dışarıya açtık
