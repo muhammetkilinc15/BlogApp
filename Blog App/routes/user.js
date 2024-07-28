b0ce94c600bf3a7e2847908c2f7e08caf35cbc3c
muhammetkilinc15 <mhmmtklnc07@gmail.com>
2024-07-28 16:21:01 +0300

    Added blog detail from database

--- Blog App/routes/user.js
+++ Blog App/routes/user.js
@@ -2,64 +2,50 @@ const express = require("express");
 const router = express.Router();
 const db = require("../data/db");
 
-const data = {
-  title: "Popüler Kurslar",
-  categories: [
-    "Web Geliştirme",
-    "Programlama",
-    "Mobil Uygulamalar",
-    "Veri Analizi",
-    "Ofis Uygulamaları",
-  ],
-};
-
 // Bir kursa ait detaya gittik
-router.get("/blogs/:blogid", function (req, res) {
-  res.render("users/blogs-detail", {
-    blogid: req.params.blogid,
-    blog: req.params.blog,
-  });
+router.get("/blogs/:blogid", async function (req, res) {
+  const id = req.params.blogid;
+  try {
+    const [blog] = await db.execute("select * from blog where idblog=?", [id]);
+    res.render("users/blog-details", {
+      title: blog[0].title,
+      blog : blog[0]
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-// Tüm Kursları blogs sayfasında listeledik
-router.get("/blogs", function (req, res) {
-  db.execute("select * from blog where isApproved=1")
-    .then((result) => {
-      db.execute("select * from category")
-        .then((data) => {
-          res.render("users/blogs", {
-            title: "Tüm Kurslar",
-            categories: data[0],
-            blogs: result[0],
-          });
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    })
-    .catch();
+// Tüm Kursları blogs sayfasında listeledik. async ve await ile kullandık
+router.get("/blogs", async function (req, res) {
+  try {
+    const [blogs] = await db.execute("select * from blog where isApproved=1");
+    const [categories] = await db.execute("select * from category");
+    res.render("users/blogs", {
+      title: "Tüm Kurslar",
+      categories: categories,
+      blogs: blogs,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // Ana Sayfada Gerekli kursları listeledik
-router.get("/", function (req, res) {
-  db.execute("select * from blog where isApproved=1 and mainPage=1")
-    .then((result) => {
-        db.execute("select * from category")
-        .then(data=>{
-            res.render("users/index", {
-                title: "Popüler Kurslar",
-                categories: data[0],
-                blogs: result[0],
-              });
-        })
-        .catch(err=>{
-            console.log(err)
-        });
-     
-    })
-    .catch((err) => {
-      console.log(err);
+router.get("/", async function (req, res) {
+  try {
+    const [blogs] = await db.execute(
+      "select * from blog where isApproved=1 and mainPage=1"
+    );
+    const [categories] = await db.execute("select * from category");
+    res.render("users/index", {
+      title: "Popüler Kurslar",
+      categories: categories,
+      blogs: blogs,
     });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router; // Dışarıya açtık
