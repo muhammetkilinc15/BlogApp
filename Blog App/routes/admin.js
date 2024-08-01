c0d19620371e2f86720eb28506581a2ea1f32789
muhammetkilinc15 <mhmmtklnc07@gmail.com>
2024-08-01 13:25:44 +0300

    Added post method

--- Blog App/routes/admin.js
+++ Blog App/routes/admin.js
@@ -17,10 +17,24 @@ router.get("/blog/create",async function(req,res){
         })
     }catch(e){
         console.log(e);
-    }
+    }   
+})
 
-
-   
+// Yeni blog eklenince bu rouuter a gelecek
+router.post("/blog/create",async function(req,res){
+   const title = req.body.title;
+   const description = req.body.Description;
+   const image = req.body.Image;
+   const category = req.body.category;
+   const mainPage = req.body.mainPage=="0n" ?  1:0;
+   const isApproved = req.body.isApproved=="0n" ?  1:0; 
+   try
+   {
+    await db.execute("insert into blog (Title,Description,Image,mainPage,isApproved,categoryId) values(?,?,?,?,?,?)",[title,description,image,mainPage,isApproved,category])  
+    res.redirect("/");
+}catch(err){
+    console.log(err)
+   }
 })
 
 
