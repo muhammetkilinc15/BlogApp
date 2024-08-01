78062129a4503603401005b0ff69d450e3df5de4
muhammetkilinc15 <mhmmtklnc07@gmail.com>
2024-08-01 12:49:27 +0300

    Datas comming dynamicly

--- Blog App/routes/admin.js
+++ Blog App/routes/admin.js
@@ -2,20 +2,36 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
+const db = require("../data/db");
+
+
+
 // admin klasörü altındaki blog-create.html sayfa yönlendirme
-router.get("/blog/create",function(req,res){
-    res.sendFile(path.join(__dirname,"../views/admin/","blog-create.html"))
+router.get("/blog/create",async function(req,res){
+    try{
+        const [category,] = await db.execute("select * from category");
+
+        res.render("admin/blog-create",{
+            title: "Add blog",
+            categories : category
+        })
+    }catch(e){
+        console.log(e);
+    }
+
+
+   
 })
 
 
 // :blogid dedik mi bir değişken olarak algılar.
 router.get("/blogs/:blogid",function(req,res){
-    res.sendFile(path.join(__dirname,"../views/admin/","blog-edit.html"))
+    res.render("admin/blog-edit")
 })
 
 
 router.get("/blogs",function(req,res){
-    res.sendFile(path.join(__dirname,"../views/admin/","blog-list.html"))
+    res.render("admin/blog-list")
 })
 
 
