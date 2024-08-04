507099b1acf692a5159cadb074da50ebe1c5036e
muhammetkilinc15 <mhmmtklnc07@gmail.com>
2024-08-04 23:07:36 +0300

    routers made simple

--- Blog App/routes/admin.js
+++ Blog App/routes/admin.js
@@ -5,296 +5,55 @@ const db = require("../data/db");
 
 const imageUpload = require("../helpers/image-upload");
 
-const fs = require("fs"); // Deleted file
-
-const Blog = require("../models/blog");
-const Category = require("../models/category");
-const { where } = require("sequelize");
+const adminController = require("../controllers/adminController");
 
 //! ########################## Category Operations ##########################
 // ************************************** Remove Category **************************************
-router.get("/categories/delete/:categoryId", async (req, res) => {
-  const categoryId = req.params.categoryId;
-  try {
-    // `await` anahtar kelimesini kullanarak veriyi çekiyoruz
-    const category = await Category.findByPk(categoryId);
+router.get(
+  "/categories/delete/:categoryId",
+  adminController.get_category_delete
+);
 
-    // `category` değişkeni null olabilir, bu yüzden bir kontrol ekledik
-    if (!category) {
-      return res.status(404).send("Category not found");
-    }
-
-    res.render("admin/category-remove", {
-      title: "Delete Category",
-      category: category,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
-router.post("/categories/delete/:categoryId", async (req, res) => {
-  const categoryId = req.body.id;
-  try {
-    const deleted = await Category.destroy({
-      where: {
-        id: categoryId,
-      },
-    });
-
-    if (deleted) {
-      return res.redirect("/admin/categories?action=delete");
-    } else {
-      return res.status(404).send("Category not found");
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Internal Server Error");
-  }
-});
+router.post(
+  "/categories/delete/:categoryId",
+  adminController.post_category_delete
+);
 
 // ************************************** Create Category **************************************
 
-router.get("/category/create", async function (req, res) {
-  try {
-    res.render("admin/category-create", {
-      title: "Add category",
-    });
-  } catch (e) {
-    console.log(e);
-  }
-});
+router.get("/category/create", adminController.get_category_create);
 
-router.post("/category/create", async function (req, res) {
-  const name = req.body.Name;
-  try {
-    await Category.create({ Name: name });
-    return res.redirect("/admin/categories?action=create");
-  } catch (err) {
-    console.log(err);
-  }
-});
+router.post("/category/create", adminController.post_category_create);
 
 // ************************************** Edit Category **************************************
-router.get("/categories/:categoryId", async (req, res) => {
-  try {
-    const categoryId = req.params.categoryId;
-    const category = await Category.findByPk(categoryId);
-    if (category) {
-      return res.render("admin/category-edit", {
-        title: "Edit Category",
-        category: category,
-      });
-    } else {
-      return res.status(404).send('Category not found');
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).send('Internal Server Error');
-  }
-});
+router.get("/categories/:categoryId", adminController.get_category_edit);
 
-
-router.post("/categories/:categoryId", async (req, res) => {
-  try {
-    const categoryId = req.body.id; // `req.params.categoryId` yerine `req.body.id` 
-    const Name = req.body.Name;
-
-    // `update` metodunu doğru şekilde kullanmalısınız
-    const updated = await Category.update(
-      { Name: Name },
-      { where: { id: categoryId } }
-    );
-
-    if (updated) {
-      return res.redirect("/admin/categories?action=edit");
-    } else {
-      return res.status(404).send('Category not found');
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
+router.post("/categories/:categoryId", adminController.post_category_edit);
 
 // ************************************** List Category **************************************
-router.get("/categories", async (req, res) => {
-  try {
-    const categories = await Category.findAll();
-    res.render("admin/category-list", {
-      title: "category-list",
-      categories: categories,
-      action: req.query.action,
-    });
-  } catch (err) {
-    console.log(err);
-  }
-});
+router.get("/categories", adminController.get_category_list);
 
 //! ########################## Blog Operations ##########################
 
 // ************************************** Remove Blog **************************************
 
-router.get("/blog/delete/:blogid", async (req, res) => {
-  const blogid = req.params.blogid;
-  try {
-    const blog = await Blog.findByPk(blogid);
-    res.render("admin/blog-remove", {
-      title: "Delete Blog",
-      blog: blog,
-    });
-  } catch (err) {
-    console.log(err);
-  }
-});
+router.get("/blog/delete/:blogid",adminController.get_blog_delete );
 
-router.post("/blog/delete/:blogid", async (req, res) => {
-  try {
-    const blogid = req.body.id;
-    const deleted = await Blog.destroy({
-      where:{
-        id : blogid
-      }
-    })
-    if (deleted) {
-      return res.redirect("/admin/blogs?action=delete");
-    } else {
-      return res.status(404).send("Blog not found");
-    }
-   
-  } catch (err) {
-    console.log(err);
-  }
-});
+router.post("/blog/delete/:blogid",adminController.post_blog_delete );
 
 // ************************************** Create New Blog **************************************
-router.get("/blog/create", async function (req, res) {
-  try {
-    const categories = await Category.findAll();
+router.get("/blog/create",adminController.get_blog_create );
 
-    res.render("admin/blog-create", {
-      title: "Add blog",
-      categories: categories,
-    });
-  } catch (e) {
-    console.log(e);
-  }
-});
-
-router.post(
-  "/blog/create",
-  imageUpload.upload.single("Image"),
-  async function (req, res) {
-    const title = req.body.title;
-    const subtitle = req.body.subtitle;
-    const description = req.body.Description;
-    const image = req.file.filename;
-    const category = req.body.category;
-    const mainPage = req.body.mainPage == "on" ? 1 : 0;
-    const isApproved = req.body.isApproved == "on" ? 1 : 0;
-    console.log(category);
-    try {
-      await Blog.create({
-        Title: title,
-        SubTitle: subtitle,
-        Description: description,
-        Image: image,
-        mainPage: mainPage,
-        isApproved: isApproved,
-        categoryId: category,
-      });
-
-      res.redirect("/admin/blogs?action=create");
-    } catch (err) {
-      console.log(err);
-    }
-  }
+router.post("/blog/create",imageUpload.upload.single("Image"),adminController.post_blog_create
 );
 
 // ************************************** Edit Blog **************************************
-router.get("/blogs/:blogid", async (req, res) => {
-  try {
-    const blogid = req.params.blogid;
-    const blog = await Blog.findByPk(blogid);
-    const categories = await Category.findAll();
-    console.log(blog.categoryId)
-    if (blog && categories) {
-      return res.render("admin/blog-edit", {
-        title: blog.title,
-        blog: blog,
-        categories: categories,
-      });
-    }
-    return res.redirect("admin/blog-edit");
-  } catch (err) {
-    console.log(err);
-  }
-});
+router.get("/blogs/:blogid",adminController.get_blog_edit );
 
-router.post(
-  "/blogs/:blogid",
-  imageUpload.upload.single("Image"),
-  async (req, res) => {
-    const blogId = req.body.id;
-    const title = req.body.Title;
-    const subtitle = req.body.SubTitle;
-    const description = req.body.Description;
-    let image = req.body.Image; // Varsayılan olarak eski resim
-    const categoryId = req.body.category;
-    const mainPage = req.body.mainPage == "on" ? 1 : 0;
-    const isApproved = req.body.isApproved == "on" ? 1 : 0;
-
-    // Yeni resim yüklenmişse, image'ı güncelle
-    if (req.file) {
-      image = req.file.filename;
-
-      // Eski resmi sil
-      fs.unlink("./public/images/" + req.body.Image, (err) => {
-        if (err) {
-          console.log(err);
-        }
-      });
-    }
-
-    try {
-      // Blog'u güncelleme
-      await Blog.update(
-        {
-          Title: title,
-          Description: description,
-          Image: image,
-          categoryId: categoryId,
-          mainPage: mainPage,
-          isApproved: isApproved,
-          SubTitle: subtitle,
-        },
-        { where: { id: blogId } }
-      );
-
-      // Güncelleme başarılıysa yönlendirme yap
-      return res.redirect("/admin/blogs?action=edit");
-    } catch (err) {
-      console.log(err);
-      // Hata durumunda bir hata sayfasına veya bir mesajla kullanıcıyı bilgilendirin
-      res.status(500).send("An error occurred while updating the blog.");
-    }
-  }
+router.post("/blogs/:blogid",imageUpload.upload.single("Image"),adminController.post_blog_edit
 );
 
 // ************************************** Blog List **************************************
-router.get("/blogs", async function (req, res) {
-  try {
-    const blogs = await Blog.findAll();
-    res.render("admin/blog-list", {
-      title: "Blog-List",
-      blogs: blogs,
-      action: req.query.action,
-    });
-  } catch (err) {
-    console.log(err);
-  }
-});
+router.get("/blogs",adminController.get_blog_list );
 
 module.exports = router; // Dışarıya açtık
--- Blog App/routes/user.js
+++ Blog App/routes/user.js
@@ -2,95 +2,20 @@ const express = require("express");
 const router = express.Router();
 const db = require("../data/db");
 
+const userController = require("../controllers/UserController");
 
-const Blog = require("../models/blog")
-const Category = require("../models/category");
 
 // ************************************** Get Course with by Category **************************************
-router.use("/blogs/category/:idCategory", async (req, res) => {
-  const id = req.params.idCategory;
-  try {
-    const blogs = await Blog.findAll({
-      where : {
-        categoryId : id
-      }
-    });
-    const categories = await Category.findAll();
-    res.render("users/blogs", {
-      title: "Tüm Kurslar",
-      categories: categories,
-      blogs: blogs,
-      selectedCategory:id
-    });
-  } catch (err) {
-    console.log(err);
-  }
-
-});
+router.use("/blogs/category/:idCategory", userController.blog_by_category);
 
 // ************************************** Go to Course Detail with its id **************************************
-router.get("/blogs/:blogid", async function (req, res) {
-  const id = req.params.blogid;
-  try {
-    const blog = await Blog.findByPk(id)
-   
-
-    if (blog) {
-      return res.render("users/blog-details", {
-        title: blog.title,
-        blog: blog,
-      });
-    }
-    res.redirect("/error");
-  } catch (err) {
-    console.log(err);
-  }
-});
+router.get("/blogs/:blogid", userController.blogs_details);
 
 // ************************************** All Course Listed **************************************
-router.get("/blogs", async (req, res) => {
-  try {
-    // Blog ve kategori verilerini veritabanından çekin
-    const blogs = await Blog.findAll();
-    const categories = await Category.findAll();
-
-    // Verileri şablona gönderin
-    res.render("users/blogs", {
-      title: "Tüm Bloglar", // Sayfa başlığı
-      categories: categories, // Tüm kategoriler
-      blogs: blogs,           // Tüm bloglar
-      selectedCategory: null  // Seçili kategori (yok)
-    });
-  } catch (err) {
-    // Hata durumunda hata mesajını konsola yazdırın
-    console.error(err);
-    // Kullanıcıya hata sayfası veya mesaj gösterin
-    res.status(500).send("An error occurred while fetching blogs and categories.");
-  }
-});
+router.get("/blogs", userController.blog_list);
 
 // ************************************** Listed Couse for Main Page **************************************
-router.get("/", async function (req, res) {
-  try {
-    const blogs = await Blog.findAll({
-      where : {
-        mainPage  : 1,
-        isApproved : 1
-      }
-    })
-    const categories = await Category.findAll({
-      raw:true
-    });
-    res.render("users/index", {
-      title: "Popüler Kurslar",
-      categories: categories,
-      blogs: blogs,
-      selectedCategory:null
-    });
-  } catch (err) {
-    console.log(err);
-  }
-});
+router.get("/", userController.index);
 
 // ************************************** Error Page **************************************
 router.get("/error", function (req, res) {
