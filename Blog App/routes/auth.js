12a97d365b600ec8fbfc63c3a0eb3c6708dd532c
muhammetkilinc15 <mhmmtklnc07@gmail.com>
2024-08-11 17:33:57 +0300

    added csrf middleware

--- Blog App/routes/auth.js
+++ Blog App/routes/auth.js
@@ -2,18 +2,20 @@ const express = require("express")
 const router = express.Router();
 
 const authController = require("../controllers/auth");
+const csrf = require("../middlewares/csrf")
+
 
 // Kayıt ol get - post metotları 
-router.get("/register",authController.get_register)
+router.get("/register",csrf,authController.get_register)
 router.post("/register",authController.post_register)
 
 
 // Login işlemi için get - post
-router.get("/login", authController.get_login);
+router.get("/login",csrf, authController.get_login);
 router.post("/login", authController.post_login);
 
 // Auth işlemleri
 
-router.get("/logout",authController.get_logout)
+router.get("/logout",csrf,authController.get_logout)
 
 module.exports = router;
