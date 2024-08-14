acc18c87d7c01e4e069b25543994c5a307d90ace
muhammetkilinc15 <mhmmtklnc07@gmail.com>
2024-08-15 00:02:37 +0300

    Added sender email adress

--- Blog App/config.js
+++ Blog App/config.js
@@ -6,6 +6,11 @@ const config = {
         user: "root",
         password: "141021Bemu",
         database: "blogdb"
+    },
+    email: {
+        username: "nodejsdeneme0715@outlook.com", // Doğru e-posta adresiniz
+        password: "19072002Fb", // Eğer 2FA etkinse, bu alana uygulama şifresi girilmeli
+        from: "nodejsdeneme0715@outlook.com" // Gönderici e-posta adresi
     }
 };
 
