a6ac6ac68ba8dec0b6f071a503a3d3813338d1d9
muhammetkilinc15 <mhmmtklnc07@gmail.com>
2024-08-17 05:35:01 +0300

    Users can access the url with theirpermission

--- Blog App/routes/admin.js
+++ Blog App/routes/admin.js
@@ -2,47 +2,49 @@ const express = require("express");
 const router = express.Router();
 
 const imageUpload = require("../helpers/image-upload");
-const isAuth = require("../middlewares/auth")
+const isAdmin = require("../middlewares/is-admin")
+
 const csrf = require("../middlewares/csrf") // csrf için 
 
 const adminController = require("../controllers/admin");
+const isModerator = require("../middlewares/is-moderator");
 
 // ************************************** Blog Routes Settings **************************************
-router.get("/blog/delete/:blogid",isAuth,csrf, adminController.get_blog_delete);
-router.post("/blog/delete/:blogid", isAuth,adminController.post_blog_delete);
+router.get("/blog/delete/:blogid",isModerator,csrf, adminController.get_blog_delete);
+router.post("/blog/delete/:blogid", isModerator,adminController.post_blog_delete);
 
-router.get("/blog/create",isAuth,csrf, adminController.get_blog_create);
-router.post("/blog/create",isAuth, imageUpload.upload.single("resim"), adminController.post_blog_create);
+router.get("/blog/create",isModerator,csrf, adminController.get_blog_create);
+router.post("/blog/create",isModerator, imageUpload.upload.single("resim"), adminController.post_blog_create);
 
-router.get("/blogs/:blogid",isAuth, csrf,adminController.get_blog_edit);
+router.get("/blogs/:blogid",isModerator, csrf,adminController.get_blog_edit);
 
-router.post("/blogs/:blogid", isAuth,imageUpload.upload.single("resim"), adminController.post_blog_edit);
-router.get("/blogs",isAuth,adminController.get_blogs);
+router.post("/blogs/:blogid", isModerator,imageUpload.upload.single("resim"), adminController.post_blog_edit);
+router.get("/blogs",isModerator,adminController.get_blogs);
 
 // ************************************** Category Routes Settings **************************************
-router.get("/category/delete/:categoryid",isAuth,csrf, adminController.get_category_delete);
-router.post("/category/delete/:categoryid", isAuth,adminController.post_category_delete);
+router.get("/category/delete/:categoryid",isAdmin,csrf, adminController.get_category_delete);
+router.post("/category/delete/:categoryid", isAdmin,adminController.post_category_delete);
 
-router.post("/categories/remove",isAuth, adminController.get_category_remove);
+router.post("/categories/remove",isAdmin, adminController.get_category_remove);
 
-router.get("/category/create",isAuth,csrf, adminController.get_category_create);
-router.post("/category/create",isAuth, adminController.post_category_create);
+router.get("/category/create",isAdmin,csrf, adminController.get_category_create);
+router.post("/category/create",isAdmin, adminController.post_category_create);
 
-router.get("/categories/:categoryid",isAuth,csrf, adminController.get_category_edit);
-router.post("/categories/:categoryid",isAuth, adminController.post_category_edit);
+router.get("/categories/:categoryid",isAdmin,csrf, adminController.get_category_edit);
+router.post("/categories/:categoryid",isAdmin, adminController.post_category_edit);
 
-router.get("/categories",isAuth, adminController.get_categories);
+router.get("/categories",isAdmin, adminController.get_categories);
 
 // ************************************** UserRoles Routes Settings **************************************
 
-router.get("/roles/:roleid", isAuth, csrf, adminController.get_role_edit);
-router.post("/roles", isAuth, adminController.post_role_edit);
+router.get("/roles/:roleid", isAdmin, csrf, adminController.get_role_edit);
+router.post("/roles", isAdmin, adminController.post_role_edit);
 
-router.post("/roles/remove",isAuth,csrf, adminController.roles_remove);
-router.get("/roles/",isAuth,csrf, adminController.get_roles);
+router.post("/roles/remove",isAdmin,csrf, adminController.roles_remove);
+router.get("/roles/",isAdmin,csrf, adminController.get_roles);
 
 
-router.get("/users",isAuth,adminController.get_user)
-router.get("/users/:userid",isAuth,csrf,adminController.get_user_edit)
-router.post("/users/:userid",isAuth,csrf,adminController.post_user_edit)
+router.get("/users",isAdmin,adminController.get_user)
+router.get("/users/:userid",isAdmin,csrf,adminController.get_user_edit)
+router.post("/users/:userid",isAdmin,csrf,adminController.post_user_edit)
 module.exports = router;
